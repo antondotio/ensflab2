97b423b2d87af3282cd2394911f867d56ef5c5d3
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-01-25 14:47:07 -0700

    lab2-inlab

--- Line.java
+++ Line.java
@@ -18,13 +18,7 @@ class Line {
     
     public String toString()
     {
-        // THIS METHOD DOESN'T WORK. AS PART OF EXERCISE-2 STUDENTS MUST FIX IT
-        // TO RETURN A STRING WITH TWO COORDINATES OF THE START AND END POINTS
-        // OF A LINE, IN THE FORMAT SHOWN IN THE EXAMPLE BELOW:
-        // Line 1: starts at (20, 30), and ends at (50, 100)
-        //
-    	String s = "Oops... Line's toString doesn't know how to use Line's toString to display"
-    			+ " the start and end point of a line coordinates...";
+		String s = "Line " + objID + ": starts at" + start.toString() + ", and ends at" + end.toString();		
     	return s;
     }
 }
--- Polygon.java
+++ Polygon.java
@@ -26,19 +26,10 @@ class Polygon {
 	
     
         public String toString() {
-            // THIS METHOD DOESN'T WORK. AS PART OF EXERCISE-2 STUDENTS MUST FIX
-            // IT TO RETURN A STRING WITH THE INFORMATION ABOUT START AND END
-            // POINTS OF N LINES OF A POLYGON. AS SHOWN IN THE EXAMPLE BELOW:
-            // The lines in polygon 1 are:
-            //   Line 1: starts at (20, 30), and ends at (50, 100)
-            //   Line 2: starts at (50, 100), and ends at (100, 30)
-            //   Line 3: starts at (100, 30), and ends at (20, 30)
-            //
-        String s = "\nOops... Polygon's toString method doesn't know how to use "
-    			+ "the Line's toString method.\n        And Line's toString"
-    			+ " method doesn't know how to use Point's toString method "
-    			+ "to\n        display its lines' start and end point coordinates. ";
-
+			String s = new String();
+			System.out.print("\nThe Lines in polygon " + objID + " are:");
+			for(Line l: polygon)
+				s += "\n" + l.toString();
     	return s;	
     }
 }
